7702f48231afbaf4fc957493d562cb3efb2d2833
Sean OReilly <seanvoreilly2@gmail.com>
2018-04-07 12:43:19 -0700

    Generalize functions

---
+++ main.py
@@ -0,0 +1,20 @@
+from zeta import *
+# from mpmath import *
+
+# API
+
+# plot functions with:
+# - product_plot(re, im, ?lim)
+# - abs_vs_n(re, im, ?lim)
+# - drdtheta_vs_n(re, im, ?lim)
+
+# build lists with:
+# - get_zeros
+# - product_spiral(re, im, ?lim) -- uses global primes for length
+# - sum_spiral(re, im, ?lim) -- uses 100000 as default value
+# - drdtheta(r) -- where r is a list of vector magnitudes
+# - criticals(f) -- where f is a list of single-valued drdt outputs
+
+# zeros = get_zeros(5)
+
+abs_vs_n(0.5, 10)
--- zeta.py
+++ zeta.py
@@ -1,32 +1,12 @@
-from math import *
+# from mpmath import *
 from functools import reduce
+from primes import *
 import matplotlib.pyplot as plt
-import sys
-from primes import primes, zeta0
+
+mp.dps = 32
 
 
-def get_primes(n):
-    filename = open("primes.txt", "w+")
-    filename.write(str(2) + ",\n")
-    primes = [2]
-    i = 3
-    while len(primes) < n:
-        valid = True
-        for p in primes:
-            if p > sqrt(i):
-                break
-            elif i % p == 0:
-                valid = False
-
-        if valid:
-            primes.append(i)
-            filename.write(str(i) + ",\n")
-
-        i = i + 1
-
-    filename.close()
-    return primes
-
+# Deprecated:
 
 def chomp(x):
     if x.endswith("\r\n"):
@@ -56,90 +36,151 @@ def reciprocal(a):
     return [a[0] / denom, -a[1] / denom]
 
 
-# tests
-assert chomp("sean\n") == "sean"
-assert chomp("sean") == "sean"
-assert one_minus_p_s(2, 0) == [1 - 1 / sqrt(2), 0]
-assert one_minus_p_s(2, 1) == [1 - cos(log(2)) /
-                               sqrt(2), sin(log(2)) / sqrt(2)]
-assert mult([1, 0], [1, 0]) == [1, 0]
-assert mult([2, 5], [3, -3]) == [21, 9]
-assert reciprocal([1, 0]) == [1, 0]
-assert reciprocal([3, 2]) == [3 / 13, -2 / 13]
+# Build Functions
 
-print "Passed all tests"
+def get_primes(n):
+    filename = open("primes.txt", "w+")
+    filename.write(str(2) + ",\n")
+    primes = [2]
+    i = 3
+    while len(primes) < n:
+        valid = True
+        for p in primes:
+            if p > sqrt(i):
+                break
+            elif i % p == 0:
+                valid = False
+
+        if valid:
+            primes.append(i)
+            filename.write(str(i) + ",\n")
+
+        i = i + 1
+
+    filename.close()
+    return primes
+# only use to regenerate txt, use global primes instead
+
+
+def get_zeros(n=-1):
+    zetadata = open("zeros1.txt", "r")
+    zeros = map(lambda line: mpf(chomp(line)), zetadata.readlines()[0:n])
+    zetadata.close()
+    print "%d zeros read" % len(zeros)
+
+    return zeros
+
+
+def product_spiral(re, im, lim=len(primes)):
+    result = []
+    product = 1
+    for p in primes[0:lim]:
+        nextterm = 1 / (1 - p**(-1 * mpc(complex(re, im))))
+        product = product * nextterm
+        result.append(product)
+
+    return result
+# to analyze deprecated version:
+# nextterm = reciprocal(one_minus_p_s(p, zeros[89]))
+# product = mult(product, nextterm)    My own version with less precision
+
+
+def sum_spiral(re, im, lim=100000):
+    result = []
+    total = 0
+    for n in range(1, lim):
+        nextterm = 1 / n**(mpc(complex(re, im)))
+        total = total + nextterm
+        result.append(total)
+
+    return result
+
+
+def drdtheta(r):
+    drdtheta = []
+    for i, r_i in enumerate(r):
+        if i == 0:
+            continue
+        drdtheta.append((r_i - r[i - 1]))
+
+    return drdtheta
+
+
+def criticals(f):
+    criticals = []
+    for i, f_i in enumerate(f):
+        if i == 0:
+            continue
+        if (f[i - 1] * f_i) <= 0:
+            criticals.append(i)
+
+    return criticals
+
+# note:
+# we need the entire number at the turn around points, not just the abs
+
+
+# Prelim Data Fetch
+
+
 # get_primes(100000)
 
-zetadata = open("zeros1.txt", "r")
-zeros = map(lambda line: float(chomp(line)), zetadata.readlines())
-zetadata.close()
 
-product_result = []
-sum_result = []
-dydx = []
-criticals = []
-product = 1
-total = 0
-# # product = [1, 0]
+# Tests
 
+def run_tests():
 
-for p in primes:
-    nextterm = 1 / (1 - p**(-complex(0.5, zeros[0])))
-    # nextterm = 1 / (1 - p**(-complex(2)))
-    product = product * nextterm
-    product_result.append(product)
-# nextterm = reciprocal(one_minus_p_s(p, zeros[89]))
-# product = mult(product, nextterm)
+    assert chomp("sean\n") == "sean"
+    assert chomp("sean") == "sean"
+    print one_minus_p_s(2, 0)
+    print [1 - 1 / sqrt(2), 0]
+    assert one_minus_p_s(2, 0) == [1 - 1 / sqrt(2), 0]
+    assert one_minus_p_s(2, 1) == [1 - cos(log(2)) /
+                                   sqrt(2), sin(log(2)) / sqrt(2)]
+    assert mult([1, 0], [1, 0]) == [1, 0]
+    assert mult([2, 5], [3, -3]) == [21, 9]
+    assert reciprocal([1, 0]) == [1, 0]
+    assert reciprocal([3, 2]) == [3 / 13, -2 / 13]
 
-# for n in range(1, 100000):
-#     # nextterm = 1 / n**(complex(0.5, 17))
-#     nextterm = 1 / n**(complex(2))
-#     total = total + nextterm
-#     sum_result.append(total)
+    print "Passed All Tests"
 
 
-# x = map(lambda z: z.real, product_result)
-y = map(lambda z: abs(z), product_result)
+# Plotting Functions
 
-for i, yi in enumerate(y):
-    if i == 0:
-        continue
-
-    dydx.append((yi - y[i - 1]))
-
-x = list(range(0, len(dydx) + 1))
-
-for i, dydxi in enumerate(dydx):
-    if i == 0:
-        continue
-    if dydx[i - 1] > 0 and dydxi < 0:
-        criticals.append(i)
-    elif dydx[i - 1] < 0 and dydxi > 0:
-        criticals.append(i)
-
-print criticals
-print map(lambda i: y[i], criticals)
-# x = map(lambda z: z.real, sum_result)
-# y = map(lambda z: z.imag, sum_result)
-# for i in range(0, len(x)):
-#     print x[i], y[i]
 
 plt.xlabel('Re')
 plt.ylabel('Im')
-
-
-fig, ax = plt.subplots()
-ax.plot(x, y)
-# ax.set_aspect('equal')
+ax = plt.subplot()
 ax.grid(True, which='both')
 
-# center = product_result[-1]
-# center = sum_result[-1]
 
-d = 2
+def product_plot(re, im, n=None):
+    product_result = product_spiral(re, im, n)
+    x = map(lambda z: z.real, product_result)
+    y = map(lambda z: z.imag, product_result)
 
-# ax.axis([center[0] - d, center[0] + d, center[1] - d, center[1] + d])
-# ax.axis([center.real - d, center.real + d, center.imag - d, center.imag + d])
-ax.axis([0, 100000, -10, 10])
+    ax.plot(x, y)
+    ax.set_aspect('equal')
+    ax.axis([-5, 5, -5, 5])
+    plt.show()
 
-plt.show()
+
+def abs_vs_n(re, im, n=None):
+    product_result = product_spiral(re, im, n)
+    x = list(range(0, len(product_result)))
+    y = map(lambda z: abs(z), product_result)
+
+    ax.plot(x, y)
+    ax.axis([0, len(x), 0, 10])
+    plt.show()
+
+
+def drdtheta_vs_n(re, im, n=None):
+    product_result = product_spiral(re, im, n)
+    y = map(lambda z: abs(z), product_result)
+    dr_dt = drdtheta(y)
+    x = list(range(0, len(dr_dt)))
+
+    ax.plot(x, dr_dt)
+    ax.axis([0, len(x), -10, 10])
+    plt.show()
